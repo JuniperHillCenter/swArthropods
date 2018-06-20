97ca5f874acb96ecf8a0f8cc87f852f8666f1e24
ajrominger <ajrominger@gmail.com>
2018-06-20 15:44:43 -0600

    updated to include min dist to road and an argument for excluding areas

--- make_candidate_points.R
+++ make_candidate_points.R
@@ -4,7 +4,7 @@ library(rgeos)
 
 
 # make a sampling area based on a bounding region and proximity to roads
-sampArea <- function(region, roads, utmZone = 13) {
+sampArea <- function(region, roads, roadBuffer = 100, exclude = NULL, utmZone = 13) {
     crs <- CRS(sprintf('+proj=utm +zone=%s +ellps=WGS84 +datum=WGS84 +units=m +no_defs', 
                        utmZone))
     origCRS <- CRS(proj4string(region))
@@ -14,6 +14,12 @@ sampArea <- function(region, roads, utmZone = 13) {
     
     sampHere <- gBuffer(roads, width = 1500)
     sampHere <- gIntersection(sampHere, region, byid = TRUE, drop_lower_td = TRUE)
+    sampHere <- gDifference(sampHere, gBuffer(roads, width = roadBuffer))
+    
+    if(!is.null(exclude)) {
+        exclude <- spTransform(exclude, crs)
+        sampHere <- gDifference(sampHere, exclude)
+    }
     
     sampHere <- spTransform(sampHere, origCRS)
     
@@ -31,7 +37,7 @@ sampPoints <- function(n, area, dmin) {
     xy <- xy[sample(nrow(xy@coords), n), ]
     
     d <- spDists(xy)
-    g <- cutree(hclust(as.dist(d)), h = 1)
+    g <- cutree(hclust(as.dist(d)), h = dmin / 1000)
     
     xy <- SpatialPointsDataFrame(xy, data = data.frame(name = 1:n, group = g), 
                                  proj4string = CRS(proj4string(area)))
@@ -41,8 +47,10 @@ sampPoints <- function(n, area, dmin) {
 
 
 # put it all together
-makePoints <- function(n, region, roads, dmin = 1, utmZone = 13) {
-    area <- sampArea(region, roads, utmZone)
+makePoints <- function(n, region, roads, roadBuffer = 100, exclude = NULL, 
+                       dmin = 1000, utmZone = 13) {
+    area <- sampArea(region = region, roads = roads, roadBuffer = roadBuffer, 
+                     exclude = exclude, utmZone = utmZone)
     pnts <- sampPoints(n, area, dmin)
     
     return(spTransform(pnts, CRS(proj4string(region))))
